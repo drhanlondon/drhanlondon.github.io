e3ce96b6ace4f1ee7b13ca7fb4c1f348a3171e70
Dr Seokhyun Han <han.s@alcacor.com>
2020-07-20 19:42:22 +0100

    initial commit

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "overledger-dlt-hyperledger-fabric-connector",
+  "version": "0.1.1",
+  "description": "Building RESTful server which will work as a client node which will access peer nodes and orderer nodes in Hyperledger Fabric blockchain network",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-07-20T15:32:55.900Z",
+    "url": "http://apidocjs.com",
+    "version": "0.24.0"
+  }
+});

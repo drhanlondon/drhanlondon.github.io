a89035ac33095a056b45b1cabf4cac05e450c9c5
Dr Seokhyun Han <han.s@alcacor.com>
2020-08-07 12:14:24 +0100

    update

--- script-collection.sh
+++ script-collection.sh
@@ -10,4 +10,4 @@ apidoc -i /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-1.4x-conn
         -o /home/drhan/Dropbox/myWork/drhanlondon.github.io
 
 apidoc -i /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-1.4x-connector/api \
-        -o /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-2.x-connector/api-doc
+        -o /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-1.4x-connector/api-doc

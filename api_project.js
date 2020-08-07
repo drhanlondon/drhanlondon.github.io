9d81e1f96ce44c1f9983f162f8291b5291a5e1f0
Dr Seokhyun Han <han.s@alcacor.com>
2020-08-07 12:12:17 +0100

    update

--- api_project.js
+++ api_project.js
@@ -1,15 +1,15 @@
 define({
   "title": "Custom apiDoc browser title",
   "url": "http://localhost:3000",
-  "name": "overledger-dlt-hyperledger-fabric-connector",
+  "name": "overledger-dlt-hyperledger-fabric-1.4x-connector",
   "version": "0.1.1",
-  "description": "Building RESTful server which will work as a client node which will access peer nodes and orderer nodes in Hyperledger Fabric blockchain network",
+  "description": "Building RESTful server which will work as a client node which will access peer nodes and orderer nodes in Hyperledger Fabric 1.4x blockchain network",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-08-05T08:31:06.164Z",
+    "time": "2020-08-07T11:10:38.030Z",
     "url": "http://apidocjs.com",
     "version": "0.24.0"
   }
--- script-collection.sh
+++ script-collection.sh
@@ -1,3 +1,13 @@
-apidoc -i /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-2.x-connector/api -o /home/drhan/Dropbox/myWork/drhanlondon.github.io
+apidoc -i /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-2.x-connector/api \
+        -o /home/drhan/Dropbox/myWork/drhanlondon.github.io
 
-apidoc -i /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-2.x-connector/api -o /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-2.x-connector/api-doc
+apidoc -i /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-2.x-connector/api \
+        -o /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-2.x-connector/api-doc
+
+#################################################################
+
+apidoc -i /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-1.4x-connector/api \
+        -o /home/drhan/Dropbox/myWork/drhanlondon.github.io
+
+apidoc -i /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-1.4x-connector/api \
+        -o /home/drhan/Dropbox/myWork/overledger-dlt-hyperledger-fabric-2.x-connector/api-doc

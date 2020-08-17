6a5ccdef7ab5e210b5a4d76c19d7d5752db9a450
Dr Seokhyun Han <han.s@alcacor.com>
2020-08-17 13:41:41 +0100

    update

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-08-17T12:37:21.068Z",
+    "time": "2020-08-17T12:41:30.521Z",
     "url": "http://apidocjs.com",
     "version": "0.24.0"
   }
